dc9edbeee6f4d001d6efa46ac2349690662ebfd7
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-12-26 21:13:35 -0500

    adding vue, webpack and skills carosal

--- laravel/app/Http/Controllers/PagesController.php
+++ laravel/app/Http/Controllers/PagesController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view('pages.index');
-    }
-}
+        $pageData = array( 
+            'title'=>"Cory's Corner",
+            'pages' => ['About', 'Projects', 'PodCasts', 'InstaGram', 'LinkedIn', 'Git' , 'Twitter'],
+            'socialMedia' => [`/about`, `/projects`, `https://anchor.fm/corys-corner`,`https://www.instagram.com/cory_hrycko/`,`https://www.linkedin.com/in/cory-hrycko/`, `https://github.com/CoryHrycko/`, `https://twitter.com/CoryHrycko`]
+            'frontEnd' => ['Html5/Css3', 'Sass/Scss', 'Bootstrap', 'Webpack/Gulp', 'JavaScript', 'React', 'Vue', 'jQuery', 'JSON', 'AJAX'],
+            'backEnd' => ['PHP-Laravel/Laravel Routes',"Java", "API's, HTTP Methods",'SQL/MYSQL/TSQL/POSTGREs','NODE.JS/NODE.JS Routes','Version Controls','Python-Flask','Advanced Statistics', ],
+
+        );
+
+		return view('pages.index') ->with($pageData) ;
+    }}
--- laravel/resources/views/pages/index.blade.php
+++ laravel/resources/views/pages/index.blade.php
@@ -1,104 +1,242 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('Layouts.app')
 
-        <title>{{config('app.name','coryscorrner')}}</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-                transition: 500ms;
-            }
-            .links > a:hover {
-                color: orange;
-                transition: 500ms;
-            }
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+<div class="flex-center position-ref full-height">
 
             <div class="content">
                 <div class="title m-b-md">
-                    Cory's Corner
+                    {{$title}}
                 </div>
                 <video  src="coryhrycko.com/clocks.mp4"></video>
 
                 <nav class="links" id="navBar">
-                    <a href="https://laravel.com/docs">About</a>
-                    <a href="https://github.com/laravel/laravel">Projects</a>
-                    <a href="https://laracasts.com">PodCasts</a>
-                    <a href="https://laravel-news.com">InstaGram</a>
-                    <a href="https://nova.laravel.com">LinkedIn</a>
-                    <a href="https://forge.laravel.com">Git</a>
-                    <a href="https://github.com/laravel/laravel">Twitter</a>
+                    @if(count($pages) > 0)
+                            @foreach ($pages as $page)
+                                    <a href="/{{$page}}">{{$page}}</a>
+                            @endforeach
+                    @endif
                 </nav>
             </div>
         </div>
-    </body>
-</html>
+<div class="position-ref full-height">
+    <div class="content2 container">
+        <div class= " about">
+            <div class= "Skills">
+                <div class="skillsBackEnd flex-center-right links2">
+                            {{-- Experimenting with Carosal --}}
+
+<div class="divC">
+
+		<input class="inputC w-100"checked id="one" name="multiples" type="radio" value="1">
+        <label class="labelC" for="one">1</label>
+
+		<input class="inputC w-100" id="two" name="multiples" type="radio" value="2">
+        <label class="labelC" for="two">2</label>
+
+        <input class="inputC w-100" id="three" name="multiples" type="radio" value="3">
+        <label class="labelC"for="three">3</label>
+
+        <input class="inputC w-100" id="four" name="multiples" type="radio" value="4">
+        <label  class="labelC"for="four">4</label>
+
+        <input class="inputC w-100" id="five" name="multiples" type="radio" value="5">
+        <label class="labelC" for="five">5</label>
+
+        <input class="inputC w-100" id="six" name="multiples" type="radio" value="6">
+        <label class="labelC" for="six">6</label>
+
+        <input class="inputC w-100" id="seven" name="multiples" type="radio" value="7">
+        <label class="labelC" for="seven">7</label>
+
+        <input class="inputC w-100" id="eight" name="multiples" type="radio" value="8">
+        <label class="labelC" for="eight">8</label>
+
+		<div class="containerC">
+			<div class="carouselC">
+                <h1 class="d-block w-100 flex-center"> PHP-Laravel/Laravel Routes</h1>
+				<h1 class="d-block w-100 flex-center"> Java</h1>
+				<h1 class="d-block w-100 flex-center"> API's, HTTP Methods</h1>
+				<h1 class="d-block w-100 flex-center"> SQL/MYSQL/TSQL/POSTGREs</h1>
+				<h1 class="d-block w-100 flex-center"> NODE.JS/NODE.JS Routes</h1>
+				<h1 class="d-block w-100 flex-center"> Version Controls</h1>
+				<h1 class="d-block w-100 flex-center"> Python-Flask</h1>
+				<h1 class="d-block w-100 flex-center"> Advanced Statistics</h1>
+			</div>
+		</div>
+        {{-- End of Caro --}}
+         {{-- Start front end caro --}}
+        <div class="divC">
+
+		<input class="inputC w-100"checked id="nine" name="multiples" type="radio" value="1">
+        <label class="labelC" for="nine">Front End</label>
+
+		<input class="inputC w-100" id="ten" name="multiples" type="radio" value="2">
+        <label class="labelC" for="ten">Front End</label>
+
+        <input class="inputC w-100" id="eleven" name="multiples" type="radio" value="3">
+        <label class="labelC"for="eleven">Front End</label>
+
+        <input class="inputC w-100" id="twelve" name="multiples" type="radio" value="4">
+        <label  class="labelC"for="twelve">Front End</label>
+
+        <input class="inputC w-100" id="thirteen" name="multiples" type="radio" value="5">
+        <label class="labelC" for="thirteen">Front End</label>
+
+        <input class="inputC w-100" id="fourteen" name="multiples" type="radio" value="6">
+        <label class="labelC" for="fourteen">Front End</label>
+
+        <input class="inputC w-100" id="fifteen" name="multiples" type="radio" value="7">
+        <label class="labelC" for="fifteen">Front End</label>
+
+        <input class="inputC w-100" id="sixteen" name="multiples" type="radio" value="8">
+        <label class="labelC" for="sixteen">Front End</label>
+
+		<div class="containerC">
+			<div class="carouselC">
+                <h1 class="d-block w-100 flex-center"> Html5-Ham/XML</h1>
+				<h1 class="d-block w-100 flex-center"> Css3-Sass/Scss/Bootstrap</h1>
+				<h1 class="d-block w-100 flex-center"> Webpack/Gulp</h1>
+				<h1 class="d-block w-100 flex-center"> JavaScript</h1>
+				<h1 class="d-block w-100 flex-center"> React-Redux/Vue/jQuery</h1>
+				<h1 class="d-block w-100 flex-center"> Version Controls</h1>
+				<h1 class="d-block w-100 flex-center"> JSON/AJAX</h1>
+				<h1 class="d-block w-100 flex-center"> Blade/WordPress</h1>
+			</div>
+		</div>
+	</div>
+
+	</div>
+
+                 {{-- End of Caro --}}
+
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="flex-center position-ref flex full-height ">
+    <div class="content3 container">
+        <div class="projects">
+            <div class="frontEnd links">
+                    @if(count($pages) > 0)
+                    @foreach ($pages as $page)
+                            <a href="/{{$page}}">{{$page}}</a>
+                    @endforeach
+            @endif
+            </div>
+            {{-- Experimenting with Carosal --}}
+            <div class="backEnd links2">
+                    @if(count($pages) > 0)
+                    @foreach ($pages as $page)
+                            <a href="/{{$page}}">{{$page}}</a>
+                    @endforeach
+            @endif
+            </div>
+            {{-- End of Caro --}}
+            <div class="multiMedia links2">
+
+            </div>
+        </div>
+    </div>
+</div>
+{{--  --}}
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://source.unsplash.com/user/erondu/800x400" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://source.unsplash.com/user/erondu/800x400" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://source.unsplash.com/user/erondu/800x400" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+{{--  --}}
+
+<div class="divC">
+
+		<input class="inputC w-100"checked id="one" name="multiples" type="radio" value="1">
+        <label class="labelC" for="one">1</label>
+
+		<input class="inputC w-100" id="two" name="multiples" type="radio" value="2">
+        <label class="labelC" for="two">2</label>
+
+        <input class="inputC w-100" id="three" name="multiples" type="radio" value="3">
+        <label class="labelC"for="three">3</label>
+
+        <input class="inputC w-100" id="four" name="multiples" type="radio" value="4">
+        <label  class="labelC"for="four">4</label>
+
+        <input class="inputC w-100" id="five" name="multiples" type="radio" value="5">
+        <label class="labelC" for="five">5</label>
+
+        <input class="inputC w-100" id="six" name="multiples" type="radio" value="6">
+        <label class="labelC" for="six">6</label>
+
+        <input class="inputC w-100" id="seven" name="multiples" type="radio" value="7">
+        <label class="labelC" for="seven">7</label>
+
+        <input class="inputC w-100" id="eight" name="multiples" type="radio" value="8">
+        <label class="labelC" for="eight">8</label>
+
+		<div class="containerC">
+			<div class="carouselC">
+                <h1 class="d-block w-100 flex-center"> Html5-Ham/XML</h1>
+				<h1 class="d-block w-100 flex-center"> Css3-Sass/Scss/Bootstrap</h1>
+				<h1 class="d-block w-100 flex-center"> Webpack/Gulp</h1>
+				<h1 class="d-block w-100 flex-center"> JavaScript</h1>
+				<h1 class="d-block w-100 flex-center"> React-Redux/Vue/jQuery</h1>
+				<h1 class="d-block w-100 flex-center"> Version Controls</h1>
+				<h1 class="d-block w-100 flex-center"> JSON/AJAX</h1>
+				<h1 class="d-block w-100 flex-center"> Blade/WordPress</h1>
+			</div>
+		</div>
+	</div>
+
+{{--  --}}
+<div class="flex-center position-ref full-height">
+    <div class="content4 container">
+        <form>
+            <input type="text" name="firstName" />
+            <input type="text" name="firstName"/>
+            <input type="email" name="email"/>
+        </form>
+    </div>
+</div>
+
+@endsection
+
