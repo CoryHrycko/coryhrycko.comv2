8a094158aabc7094a2261947a08e23c499d18964
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-12-27 10:40:48 -0500

    Adding the controller fornow

--- laravel/app/Http/Controllers/PagesController.php
+++ laravel/app/Http/Controllers/PagesController.php
@@ -6,15 +6,30 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
+
+    $pageData = array(
+        'title'=>"Cory's Corner",
+        'pages' => ('About', 'Projects', 'PodCasts', 'InstaGram', 'LinkedIn', 'Git' , 'Twitter'),
+        'socialMedias' => (`/about`, `/projects`, `https://anchor.fm/corys-corner`,`https://www.instagram.com/cory_hrycko/`,`https://www.linkedin.com/in/cory-hrycko/`, `https://github.com/CoryHrycko/`, `https://twitter.com/CoryHrycko`),
+        'frontEnd' => ('Html5/Css3', 'Sass/Scss', 'Bootstrap', 'Webpack/Gulp', 'JavaScript', 'React', 'Vue', 'jQuery', 'JSON', 'AJAX'),
+        'backEnd' => ('PHP-Laravel/Laravel Routes',"Java", "API's, HTTP Methods",'SQL/MYSQL/TSQL/POSTGREs','NODE.JS/NODE.JS Routes','Version Controls','Python-Flask','Advanced Statistics'),
+    );
     public function index(){
-        $pageData = array( 
-            'title'=>"Cory's Corner",
-            'pages' => ['About', 'Projects', 'PodCasts', 'InstaGram', 'LinkedIn', 'Git' , 'Twitter'],
-            'socialMedias' => [`/about`, `/projects`, `https://anchor.fm/corys-corner`,`https://www.instagram.com/cory_hrycko/`,`https://www.linkedin.com/in/cory-hrycko/`, `https://github.com/CoryHrycko/`, `https://twitter.com/CoryHrycko`],
-            'frontEnd' => ['Html5/Css3', 'Sass/Scss', 'Bootstrap', 'Webpack/Gulp', 'JavaScript', 'React', 'Vue', 'jQuery', 'JSON', 'AJAX'],
-            'backEnd' => ['PHP-Laravel/Laravel Routes',"Java", "API's, HTTP Methods",'SQL/MYSQL/TSQL/POSTGREs','NODE.JS/NODE.JS Routes','Version Controls','Python-Flask','Advanced Statistics', ],
-
-        );
-
 		return view('pages.index') ->with($pageData) ;
-    }}
+    }
+    public function about(){
+        return view('pages.about') ->with($pageData) ;
+    }
+    public function about(){
+        return view('pages.about') ->with($pageData) ;
+    }
+    public function about(){
+        return view('pages.about') ->with($pageData) ;
+    }
+    public function about(){
+        return view('pages.about') ->with($pageData) ;
+    }
+    public function about(){
+        return view('pages.about') ->with($pageData) ;
+    }
+}
